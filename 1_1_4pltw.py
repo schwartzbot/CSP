50368bd32a95110237f25e17d1976ed91ea5f0a1
RodriVille <rodrigo.villegas.venegas@gmail.com>
2020-09-25 13:22:14 -0400

    Done

--- 1_1_4pltw.py
+++ 1_1_4pltw.py
@@ -1,39 +1,24 @@
 import turtle as trtl
 
 painter = trtl.Turtle()
-painter.speed(0)
+painter.speed(50)
+painter.color("navy")
+painter.pensize(5)
 
-user1 = input("Enter a number")
-user2 = input("Enter another number")
-
-loop = 5
-#zero iteration
-while (loop < 5):
+loop = 0
+spiral = 3
+while (loop < 90):
     painter.goto(0,0)
-    painter.right(20)
-    painter.forward(80)
+    painter.right(200 / (spiral))
+    spiral = spiral + .5
+    painter.forward(50 + loop * 4)
     painter.pendown()
-    painter.circle(10)
+    painter.circle(15)
     painter.penup()
-    line = line + 1
-    if (loop % 18 == 0):
+    loop = loop + 1
+    if (loop % 5 == 0):
         painter.color("navy")
-    if (loop % 36 == 0):
-        painter.color("salmon")
-
-loop2 = 5
-#infinite interation
-while (loop2 > 4):
-    painter.goto(0,0)
-    painter.right(20)
-    painter.forward(80)
-    painter.pendown()
-    painter.circle(10)
-    painter.penup()
-    loop2 = loop2 + 1
-    if (loop2 % 18 == 0):
-        painter.color("navy")
-    if (loop2 % 36 == 0):
+    if (loop % 10 == 0):
         painter.color("salmon")
 
 wn = trtl.Screen()
