20098702ad9046cadb31c2c0b31c34ad03b69336
RodriVille <rodrigo.villegas.venegas@gmail.com>
2020-09-28 10:42:39 -0400

    done i think

---
+++ 1_1_4pt3.py
@@ -0,0 +1,26 @@
+import turtle as trtl
+
+painter = trtl.Turtle()
+
+painter.shape("circle")
+painter.hideturtle()
+painter.penup()
+
+y = 200
+x = -200
+while (x < 200):
+    x = x + 50
+    y = 200
+    painter.goto(x, y)
+    if (x == - 150):
+        painter.color("cyan")
+    else:
+        painter.color("magenta")
+    painter.stamp()
+    while (y > 0):
+        y = y - 50
+        painter.goto(x, y)
+        painter.stamp()
+
+wn = trtl.Screen()
+wn.mainloop()
