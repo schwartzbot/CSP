6c8f060ecf09c9c2de59e2867eb8f7934d0a6e88
RodriVille <rodrigo.villegas.venegas@gmail.com>
2020-09-28 10:25:24 -0400

    pt2

---
+++ 1_1_4pt2.py
@@ -0,0 +1,18 @@
+import turtle as trtl
+
+painter = trtl.Turtle()
+
+painter.shape("circle")
+painter.hideturtle()
+painter.penup()
+
+y = 200
+x = -100
+while (y > 0):
+    y = y - 50
+    painter.goto(x, y)
+    painter.color("blue")
+    painter.stamp()
+
+wn = trtl.Screen()
+wn.mainloop()
