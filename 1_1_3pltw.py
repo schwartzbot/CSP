25c92d5b0c545fdce23b737a39ecfef2f1554eb9
RodriVille <59237064+RodriVille@users.noreply.github.com>
2020-09-29 09:27:07 -0400

    Update 1_1_3pltw.py

--- 1_1_3pltw.py
+++ 1_1_3pltw.py
@@ -2,9 +2,7 @@ import turtle as trtl
 
 painter = trtl.Turtle()
 painter.speed(1000)
-i = 0
 
-#Stem
 painter.right(90)
 painter.penup()
 painter.goto(0, -150)
@@ -18,52 +16,53 @@ painter.right(270)
 painter.color("black")
 painter.pensize(1)
 
+row1 = 0
 painter.goto(0,0)
-while (i < 18):
+while (row1 < 18):
     painter.penup()
     painter.forward(20)
     painter.right(20)
     painter.pendown()
     painter.begin_fill()
     painter.circle(20)
-    i = i + 1
-    if (i % 2 == 0):
-        painter.fillcolor("Yellow")
+    row1 = row1 + 1
+    if (row1 % 2 == 0):
+        painter.fillcolor("magenta")
         painter.end_fill()
     else:
-        painter.fillcolor("red")
+        painter.fillcolor("green")
         painter.end_fill()
 painter.goto(0, -20)
-s = 0
-while (s < 10):
+row2 = 0
+while (row2 < 10):
     painter.penup()
     painter.forward(20)
     painter.right(35)
     painter.pendown()
     painter.begin_fill()
     painter.circle(20)
-    s = s + 1
-    if (s % 2 == 0):
-        painter.fillcolor("purple")
+    row2 = s + 1
+    if (row2 % 2 == 0):
+        painter.fillcolor("cyan")
         painter.end_fill()
     else:
-        painter.fillcolor("orange")
+        painter.fillcolor("yellow")
         painter.end_fill()
 painter.goto(0, -45)
-g = 0
-while (g < 7):
+row2 = 0
+while (row3 < 7):
     painter.penup()
     painter.forward(15)
     painter.right(60)
     painter.pendown()
     painter.begin_fill()
     painter.circle(20)
-    g = g + 1
-    if (g % 2 == 0):
-        painter.fillcolor("green")
+    row3 = row3 + 1
+    if (row3 % 2 == 0):
+        painter.fillcolor("red")
         painter.end_fill()
     else:
-        painter.fillcolor("blue")
+        painter.fillcolor("pink")
         painter.end_fill()
 
 painter.penup()
