98bf2859da2a060ddbedfd94c6193b411565c665
RodriVille <rodrigo.villegas.venegas@gmail.com>
2020-09-30 11:56:04 -0400

    done

--- main.py
+++ main.py
@@ -32,12 +32,42 @@ robot.speed(2)
 robot.showturtle()
 
 #---- TODO 1: change maze here
-wn.bgpic("maze2.png") # other file names should be maze2.png, maze3.png
+wn.bgpic("maze3.png") # other file names should be maze2.png, maze3.png
 
 #---- TODO 2: begin robot movement here
 # move robot forward with move()
 # turn robot left with turn_left()
 # sample while loop:
+i = 1
+while (i < 2):
+  j = 0
+  i = i + 1
+  move()
+  turn_left()
+  while(j < 2):
+    j = j + 1
+    turn_left()
+  l = 0
+  while(l<2):
+    move()
+    l = l +1
+  turn_left()
+  m = 0
+  while( m < 2):
+    move()
+    m = m + 1
+    if(m == 1):
+      robot.pencolor("cyan")
+  h = 0
+  while (h < 3):
+    turn_left()
+    h = h + 1
+  t = 0
+  while (t < 2):
+    move()
+    t = t + 1
+  turn_left()
+  move()
 '''
 i = 0
 while (i < 3): # forward 3
