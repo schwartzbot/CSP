77e57bf72b81da5a384ccdd1266a7bdf1acd5ab8
RodriVille <rodrigo.villegas.venegas@gmail.com>
2020-10-05 11:01:03 -0400

    step 21 done

--- a116buggyimage_step21_RV.py
+++ a116buggyimage_step21_RV.py
@@ -1,18 +1,18 @@
 import turtle as trtl
 
-x = trtl.Turtle()
-x.pensize(40)
-x.circle(20)
-w = 6
-y = 70
-z = 380 / w
-x.pensize(5)
-n = 0
-while (n < w):
-  x.goto(0,0)
-  x.setheading(z*n)
-  x.forward(y)
-  n = n + 1
-x.hideturtle()
+spider = trtl.Turtle()
+spider.pensize(40)
+spider.circle(20)
+loop = 6 
+length = 70
+angle = 380 / loop
+spider.pensize(5)
+i = 0 
+while (i < loop):
+  spider.goto(0,0)
+  spider.setheading(angle * i)
+  spider.forward(length)
+  i = i + 1
+spider.hideturtle()
 wn = trtl.Screen()
 wn.mainloop()
