acd9cbeca8c66121f866e01dc3817c01d1ddab9b
RodriVille <rodrigo.villegas.venegas@gmail.com>
2020-10-07 10:35:31 -0400

    done

--- a116_ladybug_RV.py
+++ a116_ladybug_RV.py
@@ -1,14 +1,29 @@
 #   a116_ladybug.py
 import turtle as trtl
 
-# create ladybug head
+
 ladybug = trtl.Turtle()
+
+loop = 6 
+length = 70#configure legs
+angle = 360 / loop
+ladybug.pensize(5)
+i = 0 
+while (i < loop):
+  ladybug.goto(0,-10)
+  ladybug.setheading(angle * i)
+  ladybug.forward(length)#Draw legs
+  i = i + 1
+
 ladybug.pensize(40)
+ladybug.penup()
+ladybug.goto(0, 30)
+ladybug.pendown()
 ladybug.circle(5)
 
 # and body
 ladybug.penup()
-ladybug.goto(0, -55) 
+ladybug.goto(-10, -35) 
 ladybug.color("red")
 ladybug.pendown()
 ladybug.pensize(40)
@@ -16,7 +31,7 @@ ladybug.circle(20)
 ladybug.setheading(270)
 ladybug.color("black")
 ladybug.penup()
-ladybug.goto(0, 5)
+ladybug.goto(5, 15)
 ladybug.pensize(2)
 ladybug.pendown()
 ladybug.forward(75)
@@ -24,17 +39,21 @@ ladybug.forward(75)
 # config dots
 num_dots = 1
 xpos = -20
-ypos = -55
+ypos = -40
 ladybug.pensize(10)
 
+
+
+
 # draw two sets of dots
+ladybug.pensize(10)
 while (num_dots <= 2 ):
   ladybug.penup()
   if (num_dots == 1):
     ladybug.goto(xpos, ypos)
   else:
     ypos = ypos + 20
-    xpos = xpos + 10
+    xpos = xpos + 5
     ladybug.goto(xpos, ypos)
   ladybug.pendown()
   ladybug.circle(3)
@@ -47,31 +66,6 @@ while (num_dots <= 2 ):
   xpos = ypos + 25
   xpos = xpos + 5
   num_dots = num_dots + 1
-
-loop = 8 
-length = 70#configure legs
-angle = 360 / loop
-ladybug.pensize(5)
-i = 0 
-while (i < loop):
-  ladybug.goto(0,20)
-  ladybug.setheading(angle * i + 45)
-  ladybug.forward(length)#Draw legs
-  i = i + 1
-
-ladybug.pensize(2)
-ladybug.penup()
-ladybug.pencolor("purple")
-ladybug.goto(30, 0)
-ladybug.pendown()
-ladybug.circle(4)
-ladybug.pensize(2)
-
-ladybug.penup()
-ladybug.pencolor("purple")
-ladybug.goto(35, 10)
-ladybug.pendown()
-ladybug.circle(4)
 
 ladybug.hideturtle()
 
