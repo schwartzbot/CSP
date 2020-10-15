fc2eac33345de4bf96c57c23191f270ee885cc2b
RodriVille <rodrigo.villegas.venegas@gmail.com>
2020-10-15 10:10:01 -0400

    ok it works now

--- a118_grades_RV.py
+++ a118_grades_RV.py
@@ -1,27 +1,25 @@
 #   a118_grades.py
 my_courses = ["English", "Math", "CS"]
 
-
-for course in my_courses:
-    print(course)
-    point = int(input("Enter your grade as a percentage: "))
-    try:
-        if point >= 90:
-            print("You have an A")
-        elif point >= 80:
-            print("You have an B")
-        elif point >= 70:
-            print("You have an C")
-        elif point >= 60:
-            print("You have an D")
-        else:
-            print("You are failing")
-    except:
-        print("invalid input")
-        course = my_courses[1]
-    else:
-        redo = input("do you want to redo your grades? (y/n)")
-        if ( redo == "y"):
+redo = "y"
+while ( redo == "y"):
+    for course in my_courses:
+        print(course)
+        try:
+            point = int(input("Enter your grade as a percentage: "))
+            if point >= 90:
+                print("You have an A")
+            elif point >= 80:
+                print("You have an B")
+            elif point >= 70:
+                print("You have an C")
+            elif point >= 60:
+                print("You have an D")
+            else:
+                print("You are failing")
+        except:
+            print("invalid input")
+            break
             course = my_courses[1]
-
+        redo = input("do you want to redo your grades? (y/n)")
 
