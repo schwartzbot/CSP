b02ab89b983d53b7762a71393c7827732937bc92
RodriVille <rodrigo.villegas.venegas@gmail.com>
2020-10-26 10:56:36 -0400

    person

--- a119.py
+++ a119.py
@@ -6,7 +6,7 @@ painter.pensize(5)
 
 loop = 0
 spiral = 3
-objectList = ["Background", "Mountains", "grass", "sun", "building", "roof", "windows", "door", "person", "turtle"]
+objectList = ["Background", "Mountains", "grass", "sun", "building", "roof", "door", "person", "turtle"]
 
 for thing in objectList:
     if thing == "Background":
@@ -60,6 +60,63 @@ for thing in objectList:
         painter.begin_fill()
         painter.circle(1000)
         painter.end_fill()
-
+    elif thing == "building":
+        painter.color("orange")
+        painter.pensize(5)
+        painter.penup()
+        painter.goto(100, -100)
+        painter.pendown()
+        i = 0
+        painter.begin_fill()
+        painter.right(18)
+        while (i < 4):
+            painter.forward(300)
+            painter.right(90)
+            i += 1
+        painter.end_fill()
+    elif thing == "door":
+        painter.color("brown")
+        painter.penup()
+        painter.goto(200, -50)
+        painter.pendown()
+        i = 0
+        painter.begin_fill()
+        while (i < 4):
+            painter.forward(40)
+            painter.right(90)
+            if i% 2 == 1:
+                painter.forward(40)
+            i += 1
+        painter.end_fill()
+    elif thing == "roof":
+        painter.color("maroon")
+        painter.penup()
+        painter.goto(100, 200)
+        painter.pendown()
+        painter.begin_fill()
+        painter.goto(250, 300)
+        painter.goto(400, 200)
+        painter.goto(100, 200)
+        painter.end_fill()
+    elif thing == "person":
+        painter.color("black")
+        painter.penup()
+        painter.goto(-300, -100)
+        painter.pendown()
+        painter.begin_fill()
+        painter.circle(20)
+        painter.end_fill()
+        painter.left(90)
+        painter.forward(20)
+        painter.left(90)
+        painter.forward(100)
+        painter.right(180)
+        painter.forward(50)
+        painter.right(60)
+        painter.forward(30)
+        painter.right(180)
+        painter.forward(30)
+        painter.right(60)
+        painter.forward(30)
 wn = trtl.Screen()
 wn.mainloop()
