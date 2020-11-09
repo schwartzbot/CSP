35b9bc57fd6d6480909dc4dab2333da888eae191
RodriVille <rodrigo.villegas.venegas@gmail.com>
2020-11-09 10:30:54 -0500

    ok

--- a123_apple_1RVV.py
+++ a123_apple_1RVV.py
@@ -2,11 +2,12 @@
 import turtle as trtl
 
 #-----setup-----
-apple_image = "apple.gif" # Store the file name of your shape
+apple_image = "pear.gif" # Store the file name of your shape
 
 wn = trtl.Screen()
 wn.setup(width=1.0, height=1.0)
 wn.addshape(apple_image) # Make the screen aware of the new file
+wn.bgpic("background.gif")
 
 apple = trtl.Turtle()
 
