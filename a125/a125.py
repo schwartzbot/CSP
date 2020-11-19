4366c40af572dee9766bd105b45f5e27f7782595
schwartzbot <73299408+schwartzbot@users.noreply.github.com>
2020-11-19 10:21:55 -0500

    Update a125.py

---
+++ a125/a125.py
@@ -0,0 +1,13 @@
+import turtle as trtl
+import random as rand
+import time
+
+#-----setup-----
+wn = trtl.Screen()
+wn.setup(width=.5, height=.5)
+wn.bgpic("board.png")# Make the background of the scene
+
+
+wn.listen()
+# wn.exitonclick()
+wn.mainloop()
